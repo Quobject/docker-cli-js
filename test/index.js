3762c22d4a913615dde25773246480bf61cd9f6a
MatthiasL <matthias_ludwig@yahoo.com>
2015-09-28 06:29:15 +1000

    removed dockermachineconfig.js

--- test/index.js
+++ test/index.js
@@ -3,67 +3,44 @@ var Docker = require('../lib/index.js');
 //var fs = require('fs');
 //var path = require('path');
 var should = require('chai').should();
-var machine = new require('./dockermachineconfig.js')();
+var assert = require('chai').assert;
+
+var dockermachine = require('dockermachineconfig');
 
 var config = require('../my_config.json');
 
 
-
-
-
-var options = {
-
-};
-
-describe('machine_config', function () {
-  before(function (done) {
-    // runs before all tests in this block
-    machine.config(config.DockerMachineName).then(
-       function (output) {
-         console.log('index.js machine_config output =',output);
-         done();
-       });
-  });
+describe('docker', function () {
 
   //before(function (done) {
   //  // runs before all tests in this block
-  //  machine.ls().then(
-  //    function (output) {
-  //      console.log(output);
-  //      done();
-  //    });
+  //  dockermachine.config(config.DockerMachineName).then(
+  //     function (output) {
+  //       console.log('index.js machine_config output =',output);
+  //       done();
+  //     });
   //});
 
-
-  it('should be valid json', function () {
-    config.DockerMachineName.should.be.a('string');
-    //should.exist(config.DockerMachineName);
-    console.log('DockerMachineStorePath', config.DockerMachineName);
+  it('should merge opts', function () {
+    var docker = new Docker({ a: 'a' });
+    assert.isNotNull(docker);
+    assert.equal(docker.a, 'a');
+    console.log('docker', docker);
   });
+
+
+
+  it('should merge opts', function (done) {
+    var docker = new Docker({ machinename: config.DockerMachineName });
+    console.log('docker', docker);
+    assert.isNotNull(docker);
+    docker.command('test').then(function (data) {
+      console.log('data2', data);
+      console.log('docker2', docker);
+      done();
+    });
+  });
+
 });
 
 
-
-//describe('info', function () {
-//  it('should return valid json', function (done) {
-//    //assert.equal(false, fmvalidate.printMsg()); s
-//    var docker = new Docker({ t: 't' });
-//    docker.info().then(function (data) {
-//      //console.log('test index.js result = ' + result);
-//      should.exist(data);
-//      done();
-//      //var obj = JSON.parse(result);
-//    });
-//  });
-//});
-
-//describe('info', function () {
-//  it('should call callback', function (done) {
-//    //assert.equal(false, fmvalidate.printMsg()); s
-//    var docker = new Docker({ t: 't' });
-//    docker.info(function (err, data) {
-//      should.exist(data);
-//      done();
-//    });
-//  });
-//});
