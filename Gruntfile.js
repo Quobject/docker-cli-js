b3b09c038833f2c769a4a69d776952525d86a6be
MatthiasL <matthias_ludwig@yahoo.com>
2015-09-27 16:24:06 +1000

    dockermachineconfig for testing using dockermachine data

---
+++ Gruntfile.js
@@ -0,0 +1,66 @@
+﻿var mkdirp = require('mkdirp');
+var fs = require('fs');
+
+module.exports = function (grunt) {  
+  var config = require('./my_config.json');//use config.json as example
+  grunt.log.writeln('config = ',config);
+
+  //grunt.loadTasks('./tasks');
+
+  grunt.initConfig({
+    shell: {
+      exec: {
+        options: {
+          stdout: true,
+          stderr: true
+        }
+      }
+    },
+    jshint: {
+      options: {
+        jshintrc: true,
+      },
+      target1: [
+        'Gruntfile.js',
+        './lib/**/*.js',
+        './test/**/*.js'
+      ]
+    },
+    //clean: {
+    //  keys: ['test/keys/'],
+    //  options: { force: true }
+    //},
+    //copy: {
+    //  keys: {
+    //    expand: true,
+    //    src: config.docker_machine_key_path + '*',
+    //    dest: 'test/keys/',
+    //    flatten: true,
+    //    filter: 'isFile',
+    //  },
+    //},
+    mochaTest: {
+      config: config,
+      test: {
+        options: {
+          reporter: 'spec',
+          bail: true,
+          quiet: false, // Optionally suppress output to standard out (defaults to false)
+          clearRequireCache: false // Optionally clear the require cache before running tests (defaults to false)
+        },
+        src: ['./test/**/*.js']
+        //src: [baseDir + '/test/fleetmakefileReplace_test.js']
+        //src: [baseDir + '/test/objectToArray_test.js']
+      }
+    },  
+
+  });
+
+  grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  //grunt.loadNpmTasks('grunt-contrib-clean');
+  //grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-mocha-test');
+
+  grunt.registerTask('default', ['jshint', 'mochaTest']);
+};
---
+++ test/index.js
@@ -0,0 +1,69 @@
+﻿/*global describe, it, before */
+var Docker = require('../lib/index.js');
+//var fs = require('fs');
+//var path = require('path');
+var should = require('chai').should();
+var machine = new require('./dockermachineconfig.js')();
+
+var config = require('../my_config.json');
+
+
+
+
+
+var options = {
+
+};
+
+describe('machine_config', function () {
+  before(function (done) {
+    // runs before all tests in this block
+    machine.config(config.DockerMachineName).then(
+       function (output) {
+         console.log('index.js machine_config output =',output);
+         done();
+       });
+  });
+
+  //before(function (done) {
+  //  // runs before all tests in this block
+  //  machine.ls().then(
+  //    function (output) {
+  //      console.log(output);
+  //      done();
+  //    });
+  //});
+
+
+  it('should be valid json', function () {
+    config.DockerMachineName.should.be.a('string');
+    //should.exist(config.DockerMachineName);
+    console.log('DockerMachineStorePath', config.DockerMachineName);
+  });
+});
+
+
+
+//describe('info', function () {
+//  it('should return valid json', function (done) {
+//    //assert.equal(false, fmvalidate.printMsg()); s
+//    var docker = new Docker({ t: 't' });
+//    docker.info().then(function (data) {
+//      //console.log('test index.js result = ' + result);
+//      should.exist(data);
+//      done();
+//      //var obj = JSON.parse(result);
+//    });
+//  });
+//});
+
+//describe('info', function () {
+//  it('should call callback', function (done) {
+//    //assert.equal(false, fmvalidate.printMsg()); s
+//    var docker = new Docker({ t: 't' });
+//    docker.info(function (err, data) {
+//      should.exist(data);
+//      done();
+//    });
+//  });
+//});
