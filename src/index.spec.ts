e2070d2a56feb75b129139274a199adbfe805feb
Matthias <matthias_ludwig@yahoo.com>
2018-08-24 17:06:29 +1000

    2.5.1 package upgrades

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,16 +1,15 @@
 /* tslint:disable:no-shadowed-variable */
 /* tslint:disable:no-unused-variable */
-import test = require('blue-tape');
-import * as path from 'path';
-import { Docker, Options } from './index';
+import test = require("blue-tape");
+import * as path from "path";
+import { Docker, Options } from "./index";
 
-test('docker-cli-js', t => {
+test("docker-cli-js", (t) => {
 
+  t.test("info", (t) => {
+    const docker = new Docker();
 
-  t.test('info', t => {
-    let docker = new Docker();
-
-    return docker.command('info').then(function (data) {
+    return docker.command("info").then(function(data) {
       console.log(data);
       t.ok(data);
       t.ok(data.object.server_version);
@@ -18,96 +17,95 @@ test('docker-cli-js', t => {
 
   });
 
-  t.test('build', t => {
+  t.test("build", (t) => {
 
     const options = new Options(
       /* machineName */ undefined,
-      /* currentWorkingDirectory */ path.join(__dirname, '..', 'test', 'nginx'),
+      /* currentWorkingDirectory */ path.join(__dirname, "..", "test", "nginx"),
     );
 
-    let docker = new Docker(options);
+    const docker = new Docker(options);
 
-    return docker.command('build -t nginximg .').then(function (data) {
-      console.log('data = ', data);
+    return docker.command("build -t nginximg .").then(function(data) {
+      console.log("data = ", data);
       t.ok(data);
       t.ok(data.success);
     });
   });
 
+  t.test("run", (t) => {
 
-  t.test('run', t => {
+    const docker = new Docker();
 
-    let docker = new Docker();
-
-    return docker.command('run --name nginxcont -d -p 80:80 nginximg').then(function (data) {
-      console.log('data = ', data);
+    return docker.command("run --name nginxcont -d -p 80:80 nginximg").then(function(data) {
+      console.log("data = ", data);
       t.ok(data.containerId);
     });
   });
 
-  t.test('ps', t => {
+  t.test("ps", (t) => {
 
-    let docker = new Docker();
+    const docker = new Docker();
 
-    return docker.command('ps').then(function (data) {
-      console.log('data = ', data);
+    return docker.command("ps").then(function(data) {
+      console.log("data = ", data);
       t.ok(data.containerList);
     });
   });
 
-  t.test('images', t => {
+  t.test("images", (t) => {
 
-    let docker = new Docker();
+    const docker = new Docker();
 
-    return docker.command('images').then(function (data) {
-      console.log('data = ', data);
+    return docker.command("images").then(function(data) {
+      console.log("data = ", data);
       t.ok(data.images);
     });
   });
 
-  t.test('network ls', t => {
+  t.test("network ls", (t) => {
 
-    let docker = new Docker();
+    const docker = new Docker();
 
-    return docker.command('network ls').then(function (data) {
-      console.log('data = ', data);
+    return docker.command("network ls").then(function(data) {
+      console.log("data = ", data);
       t.ok(data.network);
     });
   });
 
-  t.test('inspect', t => {
+  t.test("inspect", (t) => {
 
-    let docker = new Docker();
+    const docker = new Docker();
 
-    return docker.command('inspect nginxcont').then(function (data) {
-      console.log('data = ', data);
+    return docker.command("inspect nginxcont").then(function(data) {
+      console.log("data = ", data);
       t.ok(data.object);
     });
   });
 
-  t.test('search', t => {
+  t.test("search", (t) => {
 
-    let docker = new Docker();
+    const docker = new Docker();
 
-    return docker.command('search nginxcont').then(function (data) {
-      console.log('data = ', data);
+    return docker.command("search nginxcont").then(function(data) {
+      console.log("data = ", data);
       t.ok(data.images);
     });
   });
 
-  t.test('login success', t => {
+  t.test("login success", (t) => {
 
-    let docker = new Docker();
+    const docker = new Docker();
 
     // if this these credentials ever fail, they should be replaced with new valid ones.
-    return docker.command('login -u myusername -p mypassword').then(function(data) {
-      console.log('data = ', data);
+    return docker.command("login -u myusername -p mypassword").then(function(data) {
+      console.log("data = ", data);
 
       // if login succeeds, these tests should pass
       t.notOk(/error/.test(data));
       t.ok(data.login);
     }, function(data) {
-      console.log('data = ', data);
+      console.log("data = ", data);
 
       // if login is rejected, these tests should fail
       t.notOk(/error/.test(data));
@@ -115,18 +113,18 @@ test('docker-cli-js', t => {
     });
   });
 
-  t.test('login fail', t => {
+  t.test("login fail", (t) => {
 
-    let docker = new Docker();
+    const docker = new Docker();
 
-    return docker.command('login -u fakeUsername -p fakePassword').then(function (data) {
-      console.log('data = ', data);
+    return docker.command("login -u fakeUsername -p fakePassword").then(function(data) {
+      console.log("data = ", data);
 
       // if login succeeds, these tests should fail
       t.ok(/error/.test(data));
       t.notOk(data.login);
-    }, function (data) {
-      console.log('data = ', data);
+    }, function(data) {
+      console.log("data = ", data);
 
       // if login is rejected, these tests should pass
       t.ok(/error/.test(data));
--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,44 @@
-import * as _ from 'lodash';
-import * as child_process from 'child_process';
-import nodeify from 'nodeify-ts';
-import { cliTable2Json } from 'cli-table-2-json';
-import { DockerMachine } from 'dockermachine-cli-js';
+import * as child_process from "child_process";
+import { cliTable2Json } from "cli-table-2-json";
+import { DockerMachine } from "dockermachine-cli-js";
+import * as _ from "lodash";
+import nodeify from "nodeify-ts";
 const exec = child_process.exec;
 
-const splitLines = function (input: string): Array<string> {
-  return input.replace(/\r/g, '').split('\n');
+const splitLines = function(input: string): string[] {
+  return input.replace(/\r/g, "").split("\n");
 };
 
-const array2Oject = function (lines: Array<string>): Object {
-  return lines.reduce(function (object: any, linep) {
+const array2Oject = function(lines: string[]): any {
+  return lines.reduce(function(object: any, linep) {
     const line = linep.trim();
     if (line.length === 0) {
       return object;
     }
 
-    const parts = line.split(':');
+    const parts = line.split(":");
     let key = parts[0];
-    let value = parts.slice(1).join(':');
+    const value = parts.slice(1).join(":");
     key = _.snakeCase(key);
     object[key] = value.trim();
     return object;
   }, {});
 };
 
-
-const extractResult = function (result: any) {
+const extractResult = function(result: any) {
 
   const extracterArray = [
     {
       re: / build /,
-      run: function (resultp: any) {
+      run(resultp: any) {
         const lines = splitLines(resultp.raw);
 
-        lines.forEach(function (line: any) {
+        lines.forEach(function(line: any) {
           const re = /Successfully built (.*)$/;
           const str = line;
-          let m;
+          const m = re.exec(str);
 
-          if ((m = re.exec(str)) !== null) {
+          if (m !== null) {
             if (m.index === re.lastIndex) {
               re.lastIndex++;
             }
@@ -57,16 +56,15 @@ const extractResult = function (result: any) {
     },
     {
       re: / run /,
-      run: function (resultp: any) {
+      run(resultp: any) {
         resultp.containerId = resultp.raw.trim();
-
 
         return resultp;
       },
     },
     {
       re: / ps /,
-      run: function (resultp: any) {
+      run(resultp: any) {
         const lines = splitLines(resultp.raw);
 
         resultp.containerList = cliTable2Json(lines);
@@ -76,7 +74,7 @@ const extractResult = function (result: any) {
     },
     {
       re: / images /,
-      run: function (resultp: any) {
+      run(resultp: any) {
         const lines = splitLines(resultp.raw);
 
         //const debug = require('debug')('docker-cli-js:lib/index.js extractResult images');
@@ -88,7 +86,7 @@ const extractResult = function (result: any) {
     },
     {
       re: / network ls /,
-      run: function (resultp: any) {
+      run(resultp: any) {
         const lines = splitLines(resultp.raw);
 
         //const debug = require('debug')('docker-cli-js:lib/index.js extractResult images');
@@ -100,7 +98,7 @@ const extractResult = function (result: any) {
     },
     {
       re: / inspect /,
-      run: function (resultp: any) {
+      run(resultp: any) {
         const object = JSON.parse(resultp.raw);
 
         resultp.object = object;
@@ -110,7 +108,7 @@ const extractResult = function (result: any) {
     },
     {
       re: / info /,
-      run: function (resultp: any) {
+      run(resultp: any) {
         const lines = splitLines(resultp.raw);
         resultp.object = array2Oject(lines);
 
@@ -119,7 +117,7 @@ const extractResult = function (result: any) {
     },
     {
       re: / search /,
-      run: function (resultp: any) {
+      run(resultp: any) {
         const lines = splitLines(resultp.raw);
 
         resultp.images = cliTable2Json(lines);
@@ -129,7 +127,7 @@ const extractResult = function (result: any) {
     },
     {
       re: / login /,
-      run: function (resultp: any) {
+      run(resultp: any) {
         resultp.login = resultp.raw.trim();
 
         return resultp;
@@ -137,12 +135,12 @@ const extractResult = function (result: any) {
     },
   ];
 
-  extracterArray.forEach(function (extracter) {
+  extracterArray.forEach(function(extracter) {
     const re = extracter.re;
     const str = result.command;
-    let m;
+    const m = re.exec(str);
 
-    if ((m = re.exec(str)) !== null) {
+    if (m !== null) {
       if (m.index === re.lastIndex) {
         re.lastIndex++;
       }
@@ -163,34 +161,34 @@ export class Docker {
     }) { }
 
   public command(command: string, callback?: (err: any, data: any) => void) {
-    let docker = this;
-    let execCommand = 'docker ';
-    let machineconfig = '';
+    const docker = this;
+    let execCommand = "docker ";
+    let machineconfig = "";
 
-    const promise = Promise.resolve().then(function () {
+    const promise = Promise.resolve().then(function() {
       if (docker.options.machineName) {
 
         const dockerMachine = new DockerMachine();
 
-        return dockerMachine.command('config ' + docker.options.machineName).then(function (data) {
+        return dockerMachine.command("config " + docker.options.machineName).then(function(data) {
           //console.log('data = ', data);
           machineconfig = data.machine.config;
         });
       }
-    }).then(function () {
-      execCommand += ' ' + machineconfig + ' ' + command + ' ';
+    }).then(function() {
+      execCommand += " " + machineconfig + " " + command + " ";
 
-      let execOptions = {
+      const execOptions = {
         cwd: docker.options.currentWorkingDirectory,
         env: {
-          DEBUG: '',
+          DEBUG: "",
           HOME: process.env.HOME,
           PATH: process.env.PATH,
         },
         maxBuffer: 200 * 1024 * 1024,
       };
 
-      return new Promise(function (resolve, reject) {
+      return new Promise(function(resolve, reject) {
         //console.log('execCommand =', execCommand);
         //console.log('exec options =', execOptions);
 
@@ -204,17 +202,17 @@ export class Docker {
           resolve({ result: stdout});
         });
       });
-    }).then(function (data: { result: string }) {
+    }).then(function(data: any) {
       //console.log('data:', data);
-      let result = {
+      const result = {
         command: execCommand,
         raw: data.result,
       };
       return extractResult(result);
 
-      });
+    });
 
-      return nodeify(promise, callback);
+    return nodeify(promise, callback);
   }
 }
 
@@ -226,4 +224,3 @@ export interface IOptions {
 export class Options implements IOptions {
   public constructor(public machineName?: string, public currentWorkingDirectory?: string) { }
 }
-
